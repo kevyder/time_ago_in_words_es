7dd8d470c3e05be79ce2b4ed748f476e3d9a29c0
Kevin <kyrodriguez97@gmail.com>
2017-12-08 09:41:26 -0500

    Adding module to existing class

--- lib/time_ago_in_words_es.rb
+++ lib/time_ago_in_words_es.rb
@@ -1,5 +1,7 @@
 require 'time_ago_in_words_es/version'
+require 'time_ago_in_words_es/engine' if defined?(Rails) && defined?(Rails::Engine)
+require 'time_ago_in_words_es/action_view_methods' if defined? ActionView
 
 module TimeAgoInWordsEs
-  # Your code goes here...
+  ActionView::Base.send :include, ActionViewMethods
 end
